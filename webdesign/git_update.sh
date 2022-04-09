d58e0786d37ce4db0a11f31efffc473171fa4299
root <root@DESKTOP-HOB3O8I.localdomain>
2022-04-09 00:09:01 -0500

    Latest Index Update

--- webdesign/git_update.sh
+++ webdesign/git_update.sh
@@ -3,7 +3,7 @@
 
 # This will automatically update the index of my web server. I am to cron tab the shell command to execute.
 git add -A
-git  commit -a -m "Latest Index Update" 
+git commit -a -m "Latest Index Update" 
 git pull origin main
 git push origin main
 
